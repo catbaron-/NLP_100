c9b9d48f61ebb74c74e772634ca4b17a7f06dec5
catbaron- <catbaron@live.cn>
2016-12-14 14:24:19 +0900

    Added 1.05

---
+++ chapter.1/1.05.py
@@ -0,0 +1,26 @@
+# 05. n-gram
+# 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
+
+import re
+
+def n_gram(s_list, n):
+    res = []
+    if len(s_list) < n:
+        return [s_list]
+    gram = s_list[:n]
+    for i in range(n, len(s_list)):
+        res.append(gram)
+        gram = gram[1:]
+        gram.append(s_list[i])
+    res.append(gram)
+    return res
+
+def main():
+    s = "I am an NLPer"
+    print(n_gram(list(s), 2))
+
+    words = re.split(r'[\s,\.]', s)
+    print(n_gram(words, 2))
+
+if __name__ == '__main__':
+    main()
