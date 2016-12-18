a27b78a342f72d0fade423ed07b2531048f1ba85
catbaron- <catbaron@live.cn>
2016-12-18 16:10:23 +0900

    Added json file

--- chapter.2/2.19.py
+++ chapter.2/2.19.py
@@ -1,1 +1,14 @@
-2.19.py
+# 19. 各行の1コラム目の文字列の出現頻度を求め，出現頻度の高い順に並べる
+# 各行の1列目の文字列の出現頻度を求め，その高い順に並べて表示せよ．確認にはcut, uniq, sortコマンドを用いよ．
+import re
+
+with open("hightemp.txt", 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+freq = {}
+for line in lines:
+    column = re.split(r'\s', line)[0]
+    freq[column] = freq.get(column, 0) + 1
+
+for c in sorted(freq.items(), key = lambda i:i[1], reverse=True):
+    print("%s %.4f" % (c[0], c[1]/len(lines)))
